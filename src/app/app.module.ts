c4281241f1391fb472a5d0c4c3b86cf7afeb3029
T Gordo <13937751+tlavon@users.noreply.github.com>
2018-10-24 21:39:57 -0400

    Basic

---
+++ src/app/app.module.ts
@@ -0,0 +1,47 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ServicesComponent } from './services/services.component';
+import { ContactComponent } from './contact/contact.component';
+
+const appRoutes: Routes = [
+
+{
+path: 'about',
+component: AboutComponent
+},
+{
+path: 'home',
+component: HomeComponent
+},
+{
+path: 'services',
+component: ServicesComponent
+},
+{
+path: 'contact',
+component: ContactComponent
+}
+
+];
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ServicesComponent,
+    ContactComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
+
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
